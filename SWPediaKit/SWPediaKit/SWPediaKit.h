59e7cdb94135e579130a96b48205d7e8eb9954f4
Emilio Pavia <emilio.pavia@gmail.com>
2021-02-17 17:33:31 +0100

    Initial commit

---
+++ SWPediaKit/SWPediaKit/SWPediaKit.h
@@ -0,0 +1,18 @@
+//
+//  SWPediaKit.h
+//  SWPediaKit
+//
+//  Created by Emilio Pavia on 17/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SWPediaKit.
+FOUNDATION_EXPORT double SWPediaKitVersionNumber;
+
+//! Project version string for SWPediaKit.
+FOUNDATION_EXPORT const unsigned char SWPediaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWPediaKit/PublicHeader.h>
+
+
