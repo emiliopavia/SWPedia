a13950a0b226402aa9383b4eaf1be86b4a82a8cf
Emilio Pavia <emilio.pavia@gmail.com>
2021-02-20 17:18:59 +0100

    Use URLCache as a lightweight persistence storage for offline browsing

--- SWPediaKit/SWPediaKit/Networking/HTTPClient.swift
+++ SWPediaKit/SWPediaKit/Networking/HTTPClient.swift
@@ -37,11 +37,21 @@ public class HTTPClient {
 
 private extension HTTPClient {
     func response(for request: URLRequest) -> Observable<(response: HTTPURLResponse, data: Data)> {
+        let cache = session.configuration.urlCache
+        
         return Observable.create { observer in
 
             let task = self.session.dataTask(with: request) { data, response, error in
                 guard error == nil, let httpResponse = response as? HTTPURLResponse else {
                     // (e.g. offline)
+                    if let cachedResponse = cache?.cachedResponse(for: request),
+                       let httpResponse = cachedResponse.response as? HTTPURLResponse,
+                       httpResponse.isSuccess {
+                        observer.on(.next((httpResponse, cachedResponse.data)))
+                        observer.on(.completed)
+                        return
+                    }
+                    
                     observer.onError(APIError.networkError(error as NSError?))
                     return
                 }
@@ -55,6 +65,9 @@ private extension HTTPClient {
                     observer.onError(APIError.badResponse)
                     return
                 }
+                
+                cache?.storeCachedResponse(CachedURLResponse(response: httpResponse, data: data),
+                                           for: request)
                 
                 observer.on(.next((httpResponse, data)))
                 observer.on(.completed)
