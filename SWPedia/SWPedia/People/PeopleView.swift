7cdce4758c73425bf3f2a493f2f4cfce17641bc1
Emilio Pavia <emilio.pavia@gmail.com>
2021-02-20 16:23:20 +0100

    Adjust search bar layout

--- SWPedia/SWPedia/People/PeopleView.swift
+++ SWPedia/SWPedia/People/PeopleView.swift
@@ -33,8 +33,8 @@ class PeopleView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        collectionView.contentInset.top = searchBar.frame.height
-        collectionView.verticalScrollIndicatorInsets.top = searchBar.frame.height
+        collectionView.contentInset.top = searchBar.frame.height - 1
+        collectionView.verticalScrollIndicatorInsets.top = searchBar.frame.height - 1
     }
     
     private func addCollectionView() {
@@ -55,7 +55,7 @@ class PeopleView: UIView {
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
-            searchBar.topAnchor.constraint(equalToSystemSpacingBelow: safeAreaLayoutGuide.topAnchor, multiplier: 1.0),
+            searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: -1),
             searchBar.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
         searchBar.sizeToFit()
